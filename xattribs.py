829a86a06c51fdd2b0294c6ed2b390848c22a7e0
Kinga Kieczkowska <k.kieczkowska@gmail.com>
2020-06-20 16:10:23 +0100

    Improved error handling and argument parsing
    - flags into positional arguments
    - improved error handling coverage

--- xattribs.py
+++ xattribs.py
@@ -1,10 +1,10 @@
 import xattr
 import os
 import datetime as dt
-import time
-from biplist import *
 import sqlite3
 import argparse
+import sys
+import json
 
 
 def hex_to_int(string):
@@ -12,75 +12,81 @@ def hex_to_int(string):
     return int(string,16)
 
 def list_airdropped_files(directory):
-    airdropped_files = {}
-    non_aidr = []
-    for file in os.listdir(directory):
-        try:
-            quarantine = xattr.getxattr("{}/{}".format(directory, file), "com.apple.quarantine")
-            quarantine = quarantine.decode("utf-8")
 
-            if "sharingd" in quarantine:
-                temp = {}
-                time = quarantine.split(";")[1]
-                unixTS = 978307200
-                date = dt.datetime.fromtimestamp(hex_to_int(time))
-                date = str(date)
+        airdropped_files = {}
+        noq_files = []
+        for file in os.listdir(directory):
+            try:
+                quarantine = xattr.getxattr("{}/{}".format(directory, file), "com.apple.quarantine")
+                quarantine = quarantine.decode("utf-8")
+                if "sharingd" in quarantine:
+                    temp = {}
+                    time = quarantine.split(";")[1]
+                    unixTS = 978307200
+                    date = dt.datetime.fromtimestamp(hex_to_int(time))
+                    date = str(date)
 
-                temp["file_name"] = file
-                temp["file_path"] = "{}{}".format(directory, file)
-                temp["download_time"] = date
+                    temp["file_name"] = file
+                    temp["file_path"] = "{}{}".format(directory, file)
+                    temp["download_time"] = date
 
-                airdropped_files[quarantine.split(";")[-1]] = temp
-        except Exception as error:
-            #continue;
-            if "Attribute not found" in str(error):
-                non_aidr.append(file)
+                    airdropped_files[quarantine.split(";")[-1]] = temp
 
-            #print("ERROR: ", error)
-            continue;
-    print("Files / directories in the given directory which don't have the quarantine attribute: \n", non_aidr)
-    return airdropped_files
+
+            except FileNotFoundError:
+                print("#### ERROR ####\nOops. Are you sure the given directory exists?")
+                sys.exit(1)
+            except OSError as error:
+                noq_files.append(file)
+        print("Files in {} without the com.apple.quarantine attribute: {}\n".format(directory, noq_files))
+        return airdropped_files
 
 # feel free to complete this with your values for faster usage
 path = ""
 path2db = ""
 pretty_print = False
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dir', default=path,
-                    help='path to directory to check')
-parser.add_argument('--db', default=path2db,
-                    help='path to db')
+parser = argparse.ArgumentParser(description='''xattribs - a tool to view metadata of AirDropped files in a given directory.
+Requires the path to the QuarantineEvents database and the directory of files to be checked.''')
+parser.add_argument('dir', default=path,
+                    help='path to directory containing files of interest')
+parser.add_argument('db', default=path2db,
+                    help='''path to the QuarantineEvents database;
+                    it is usually /Users/<username>/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV2''')
 parser.add_argument('--pretty-print', action='store_true',
-                    help='pretty print flag')
+                    help='human-readable output')
+parser.add_argument('--json', action='store_true',
+                    help='JSON output')
 args = parser.parse_args()
 path = args.dir
 path2db = args.db
 pretty_print = args.pretty_print
+json_print = args.json
 
 file_list = list_airdropped_files(path)
-conn = sqlite3.connect(path2db)
-conn.row_factory = sqlite3.Row
-cursor = conn.execute("SELECT LSQuarantineEventIdentifier as EventID, datetime(LSQuarantineTimeStamp + strftime('%s','2001-01-01'), 'unixepoch') as TimestampUTC, LSQuarantineSenderName as SenderName FROM LSQuarantineEvent WHERE LSQuarantineAgentName LIKE 'sharingd';");
+try:
+    conn = sqlite3.connect(path2db)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.execute("SELECT LSQuarantineEventIdentifier as EventID, datetime(LSQuarantineTimeStamp + strftime('%s','2001-01-01'), 'unixepoch') as TimestampUTC, LSQuarantineSenderName as SenderName FROM LSQuarantineEvent WHERE LSQuarantineAgentName LIKE 'sharingd';");
 
-for row in cursor:
-    try:
+    for row in cursor:
         if row[0] in file_list.keys():
             file_list[row[0]]["sender_name"] = row[2]
 
-    except Exception as error:
-        #continue;
-        print("ERROR: ", error)
+    if pretty_print == True:
+        for file in file_list:
+            uid = file
+            print('''\nFile {}
+            Path: {}
+            Download time: {}
+            Sender name: {}'''.format(file_list[uid]["file_name"],
+                                      file_list[uid]["file_path"],
+                                      file_list[uid]["download_time"],
+                                      file_list[uid]["sender_name"]))
+    elif json_print == True:
+        print(json.dumps(file_list))
 
-if pretty_print == True:
-    for file in file_list:
-        uid = file
-        print('''\nFile {}
-        Path: {}
-        Download time: {}
-        Sender name: {}'''.format(file_list[uid]["file_name"],
-                                  file_list[uid]["file_path"],
-                                  file_list[uid]["download_time"],
-                                  file_list[uid]["sender_name"]))
-else:
-    print(file_list)
+except sqlite3.OperationalError:
+    print("#### ERROR ####\nOops. You're right you gave the right path to db?")
+except FileNotFoundError:
+    print("#### ERROR ####\nOops. Are you sure the given directory exists?")
